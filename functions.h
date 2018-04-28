cf7731b607a5bc69e63126315a19c1946d7cd2ee
Alex Shamroe <ajsc77@mst.edu>
2018-04-28 17:49:07 -0500

    finished boundry functions

---
+++ functions.h
@@ -0,0 +1,29 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+double function1(const double y)
+{
+	y = 0;
+	return y;
+}
+
+double function2(const double x)
+{
+	return (1 - 4 * ((x - .5) * (x - .5)));
+}
+
+
+double function3(const double y)
+{
+	y = 0;
+	return y;
+}
+
+double function4(const double x)
+{
+	x = 0;
+	return x;
+}
+
+
+#endif

4d73180a58e05c72ec810f75d0dd038ff1cbc564
Justin M Spencer <justinmspencer@Justins-MacBook-Air-2.local>
2018-04-28 17:05:19 -0500

    implmented mesh

--- mesh.h
+++ mesh.h
@@ -5,16 +5,27 @@
 
 /*
 # Mesh class header
-# By: Ari Sherman & Alex
+# By: Ari Sherman & Alexn Shamore
 # Class: CS5201 final project
 # Date: 5.7.18
 */
-#ifndef MYARRAY_H
-#define MYARRAY_H
-
+#ifndef MESH_H
+#define MESH_H
 #include <iostream>
 
 using namespace std;
 
 template <typename T>
-class MyArray;
+class mesh
+{
+	private:
+		symMatrix<T> mesh_mat;
+		MyArray<T> mesh_vec;
+		T useFunc(const T x ,const T y);
+
+	public:
+		void MakeTheMartix(const int size);
+		void MakeTheVec(const int size);
+		symMatrix<T> & getMesh_mat(return mesh_mat );
+		MyArray<T> & getMesh_vec(return mesh_vec );
+}
